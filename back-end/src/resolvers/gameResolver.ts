f47de7236513324ec6da9bb760b24eb61ea245f2
Arthur Teatree <71916438+R-33@users.noreply.github.com>
2021-04-04 20:07:09 +0930

    joinGame resolver function and createGame resolver funciton updated

--- back-end/src/resolvers/gameResolver.ts
+++ back-end/src/resolvers/gameResolver.ts
@@ -1,9 +1,10 @@
 import { Game } from "../entites/Game";
-import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import { User } from "../entites/User";
 import { MyContext } from "../types";
-import { getMongoManager, getMongoRepository } from "typeorm";
+import { getMongoRepository } from "typeorm";
 import { ObjectId } from "mongodb";
+import { isAuth } from "../utils/isAuth";
 
 @Resolver()
 export class gameResolver {
@@ -14,13 +15,11 @@ export class gameResolver {
 
   @Mutation(() => User, { nullable: true })
   async createUser(@Arg("username") username: string, @Ctx() { req }: MyContext): Promise<User | null> {
-    const manager = getMongoManager();
-
     const user = new User();
     user.nickname = username;
 
     try {
-      await manager.save(user);
+      await getMongoRepository(User).save(user);
     } catch (err) {
       return null;
     }
@@ -43,12 +42,100 @@ export class gameResolver {
   }
 
   @Mutation(() => Game, { nullable: true })
-  async createGame(): Promise<Game | null> {
-    const game = await Game.findOne();
+  @UseMiddleware(isAuth)
+  async createGame(@Ctx() { req }: MyContext): Promise<Game | null> {
+    const game = new Game();
+    // GameBoard: 0 means empty, 1 means player1/creator, 2 means joiner of game
+    game.gameBoard = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+    ]; // 7 by 6
+
+    const user1 = await getMongoRepository(User).findOne({ where: { _id: new ObjectId(req.session.userId) } });
+
+    if (!user1) {
+      throw new Error("Oh no! Something has gone terribly wrong, but how!?!?");
+    }
+
+    game.user1 = user1;
+    game.user1Id = game.user1._id;
+    game.whoseMove = user1._id;
+    game.moveNum = 0;
+
+    try {
+      await getMongoRepository(Game).save(game);
+    } catch (err) {
+      throw new Error(err as string);
+    }
+
+    return game;
+  }
+
+  @Mutation(() => Game, { nullable: true })
+  @UseMiddleware(isAuth)
+  async joinGame(@Ctx() { req }: MyContext, @Arg("gameId") gameId: string): Promise<Game | null> {
+    const _id: ObjectId = new ObjectId(gameId);
+    console.log("gameId", _id, typeof _id);
+
+    const game = await getMongoRepository(Game).findOne({ where: { _id } });
 
     if (!game) {
       return null;
     }
+
+    if ((new ObjectId(req.session.userId)).equals(game.user1Id)) {
+      throw new Error("Trying to join own game");
+    }
+
+    const joiningUserId = new ObjectId(req.session.userId)
+    const joiningUser = await getMongoRepository(User).findOne({ where: { _id: joiningUserId } });
+
+    if (!joiningUser) {
+      throw new Error("Oh no! Something has gone terribly wrong, but how!?!?");
+    } 
+
+    game.user1 = await getMongoRepository(User).findOne({ where: { _id: new ObjectId(game.user1Id) } }) as User;
+    game.user2 = joiningUser;
+    game.user2Id = joiningUserId
+
+    await getMongoRepository(Game).updateOne({_id}, { $set: { user2: joiningUser, user2Id: joiningUserId}})
+
+    return game;
+  }
+
+  @Query(() => Game)
+  @UseMiddleware(isAuth)
+  async fetchGameInfos(@Arg("gameId") gameId: string, @Ctx() { req }: MyContext,): Promise<Game | null> {
+    const _id: ObjectId = new ObjectId(gameId);
+    console.log("gameId", _id, typeof _id);
+
+    const game = await getMongoRepository(Game).findOne({ where: { _id } });
+
+    if (!game) {
+      return null;
+    }
+
+    const user1 = await getMongoRepository(User).findOne({ where: { _id: new ObjectId(game.user1Id) } });
+    if (!user1) {
+      throw new Error("Oh no! Something has gone terribly wrong, but how!?!?");
+    }
+    game.user1 = user1;
+
+    const user2 = await getMongoRepository(User).findOne({ where: { _id: new ObjectId(game.user2Id) } });
+    if (!user2) {
+      game.user2 = null;
+    } else {
+      game.user2 = user2;
+    }
+
+    if (!(req.session.userId)?.equals(game.user1Id) || !(req.session.userId)?.equals(game.user2Id)) {
+      throw new Error("Hmm pretty sus, you got any form of identification on you?");
+    }
+
     return game;
   }
 }
