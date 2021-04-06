07daffbbc6089c15d5d6b713d8b32d6e148e719a
Arthur Teatree <71916438+R-33@users.noreply.github.com>
2021-04-06 19:40:37 +0930

    added Movepiece resolver with so-far working umpire

--- back-end/src/resolvers/gameResolver.ts
+++ back-end/src/resolvers/gameResolver.ts
@@ -1,5 +1,5 @@
 import { Game } from "../entites/Game";
-import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import { User } from "../entites/User";
 import { MyContext } from "../types";
 import { getMongoRepository } from "typeorm";
@@ -19,12 +19,11 @@ export class gameResolver {
     user.nickname = username;
 
     try {
-      await getMongoRepository(User).save(user);
+      const savedUser = await getMongoRepository(User).save(user);
+      req.session.userId = savedUser._id;
     } catch (err) {
       return null;
     }
-
-    req.session.userId = user._id;
 
     return user;
   }
@@ -101,6 +100,10 @@ export class gameResolver {
       throw new Error("Trying to join own game");
     }
 
+    if (game.user2 === null) {
+      throw new Error("Game full");
+    }
+
     const joiningUserId = new ObjectId(req.session.userId);
     const joiningUser = await getMongoRepository(User).findOne({ where: { _id: joiningUserId } });
 
@@ -151,8 +154,91 @@ export class gameResolver {
 
   @Mutation(() => Game)
   @UseMiddleware(isAuth)
-  async movePiece () {
+  async movePiece(
+    @Arg("gameId") gameId: string,
+    @Arg("gameBoard", () => [[Int]]) proposedGameBoard: number[][],
+    @Ctx() { req }: MyContext
+  ): Promise<Game | null> {
+    if (proposedGameBoard.length !== 6 /* row check*/ || proposedGameBoard[0].length !== 7 /* column check*/) {
+      throw new Error("GameBoard malformed :yikes:");
+    }
 
+    const _id: ObjectId = new ObjectId(gameId);
+
+    const game = await getMongoRepository(Game).findOne({ where: { _id } });
+
+    if (!game) {
+      return null;
+    }
+
+    if (!req.session.userId?.equals(game.user1Id) && !req.session.userId?.equals(game.user2Id)) {
+      throw new Error("Hmm pretty sus, you got any form of identification on you?");
+    }
+
+    if (req.session.userId?.equals(game.whoseMove)) {
+      throw new Error("Not your move mr hacker!");
+    }
+
+    if (!isMoveLegal(game.gameBoard, proposedGameBoard, req.session.userId?.equals(game.user1Id) ? 1 : 2)) {
+      throw new Error("Illegal Move Homie");
+    }
+
+    //  console.log("not illegal")
+
+    await getMongoRepository(Game).updateOne({ _id }, { $set: { gameBoard: proposedGameBoard } });
+
+    return game;
+  }
+}
+
+const isMoveLegal = (curr: number[][], proposed: number[][], userColor: number): boolean => {
+  let differences: number[][] = [];
+
+  //finding differences between boards
+  for (let i = 0; i < curr.length; i++) {
+    for (let j = 0; j < curr[0].length; j++) {
+      if (curr[i][j] !== proposed[i][j]) {
+        differences.push([i, j]);
+      }
+    }
   }
 
-}
+  if (differences.length > 1 || differences.length == 0) {
+    console.log("no move or hacked board");
+    return false;
+  }
+
+  if (proposed[differences[0][0]][differences[0][1]] !== userColor){
+    console.log("move color bug")
+    return false;
+  }
+
+  const y: number = differences[0][0];
+  const x: number = differences[0][1];
+
+  // console.log("curr", curr)
+  // console.log("proposed", proposed)
+
+  // console.log("y: ", y, "x: ", x)
+
+  if (curr[y][x] !== 0) {
+    //move is replacing a piece / illegal move
+    console.log("move is replacing a piece");
+    return false;
+  }
+  if (y !== 5 /* placed piece on bottom of board */ || curr[y + 1][x] === 0 /* makes sure piece isnt floating */) {
+    console.log("air move or some shit");
+    return false;
+  }
+
+  /* GAMEBOARD EXAMPLE
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 2],
+  [1, 0, 0, 1, 0, 0, 2],
+  */
+
+  return true;
+};
