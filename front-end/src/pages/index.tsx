ef6ec8017a8ccfb953aee1538d6ebcc26d1d0571
Arthur Teatree <71916438+R-33@users.noreply.github.com>
2021-04-08 01:49:31 +0930

    homepage ui redesign

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import Grid from "@material-ui/core/Grid";
 import { Button, Paper, TextField, Box } from "@material-ui/core";
 import styles from "./index.module.css";
 import { gql, useMutation, useQuery } from "@apollo/client";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
+import ReCAPTCHA from "react-google-recaptcha";
 
 interface indexProps {}
 
 const index: React.FC<indexProps> = ({}) => {
-
-  const router = useRouter()
+  const router = useRouter();
+  const reRef= useRef<ReCAPTCHA>();
   const makeUser = gql`
     mutation CreateUser($username: String!) {
       createUser(username: $username) {
@@ -22,20 +23,30 @@ const index: React.FC<indexProps> = ({}) => {
   const [makeUser1, { data }] = useMutation(makeUser);
 
   return (
-    <div className={styles.centerdiv}>
-      <h1 className={styles.titlescreen}>Welcome to Connect4Online.xyz!</h1>
-      <Button
-        color='primary'
-        variant='contained'
-        size='large'
-        onClick={async () => {
-          console.log(await (await makeUser1({ variables: { username: "testuser1" } })).data);
-          router.push("/");
-        }}
-      >
-        Click to start a game!
-      </Button>
-    </div>
+    <>
+      <div className={styles.gradientBG}>
+        <div style={{ display: "grid", placeItems: "center", left: 0, right: 0, top: 0, bottom: 0, position: "fixed" }}>
+          <div style={{ display: "grid" }}>
+            <div style={{ gridRow: 1, marginBottom: "2vh" }}>
+              <div className={styles.titlescreen}>Welcome To</div>
+              <div className={styles.titlescreen}>ConnectFourOnline.xyz</div>
+            </div>
+            <div style={{ gridRow: 2, display: "grid"}} className={styles.startGameWrapper}>
+              <div className={styles.formText} style={{ gridRow: 1, paddingTop: "20px"}}>
+                your nickname:
+              </div>
+              <input className={styles.nickNameInput} style={{ gridRow: 2, paddingTop: "20px"}} />
+              <button className={styles.submitButton} style={{ gridRow: 3}}>Create Game!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ReCAPTCHA
+                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+                size="invisible"
+                ref={reRef}
+              />
+    </>
   );
 };
 
