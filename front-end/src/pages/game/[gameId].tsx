466d3271dc4e6d4dfa6e0a4c90020452954b0546
Arthur Teatree <71916438+R-33@users.noreply.github.com>
2021-04-08 17:04:32 +0930

    update

--- front-end/src/pages/game/[gameId].tsx
+++ front-end/src/pages/game/[gameId].tsx
@@ -1,53 +1,39 @@
-import { Grid, Paper } from "@material-ui/core";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import { animated, useSpring } from "react-spring";
-import PiecesAndButtons from "../../components/PiecesAndButtons";
 import styles from "./gamePage.module.css";
-import { io, Socket } from 'socket.io-client';
+import { io, Socket } from "socket.io-client";
+import stylesI from "../index.module.css";
 
+const gamePage: NextPage<{ gameId: string }> = ({ gameId }) => {
+  const [urMove, setUrMove] = useState<boolean>();
 
-const gamePage: NextPage<{ gameId: string }> = ({gameId}) => {
+  const socket = io("http://localhost:4000/");
 
-  const [urMove, setUrMove] = useState<boolean>()
-  
-
-  const socket = io("http://localhost:4000/")
-
-  socket.emit('joinRoom', {nickname: "HARDCODEDNICKNAME", roomId: gameId})
+  socket.emit("joinRoom", { nickname: "HARDCODEDNICKNAME", roomId: gameId });
 
   socket.on("moveCompleted", () => {
-    setUrMove(false)
-  })
-
+    setUrMove(false);
+  });
 
   const playerColor = "red";
 
   return (
-    <div
-      style={{
-        backgroundColor: "#262626",
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-        position: "fixed",
-      }}
-    >
-      <div className={styles.mainGrid}>
-        <div className={styles.middleWrapper}>
-          <div className={styles.boardSeperateGrid}>
-            <div className={styles.imgWrapper}>
-              <img src='../../../static/board.svg' alt='Error' className={styles.board} />
+    <div className={stylesI.gradientBG}>
+      <div className={styles.boardGrid}>
+        <div style={{ gridColumn: 4 }} className={styles.boardUIWrapper}>
+          fsdfsdfsdfsfsdfsdfsdfsdfsdfsdf
+        </div>
+        <div style={{ gridColumn: 6, display: "grid" }} className={styles.sideUIWrapper}>
+          <div style={{ gridRow: 1 }} className={styles.playerUIWrapper}>
+            hhh
+          </div>
+          <div style={{ gridRow: 2 }} className={styles.textChatWrapper}>
+            <div className={styles.sendWrapper}>
+              <input className={styles.textChatArea}></input>
+              <svg className={styles.textChatButton}></svg>
             </div>
           </div>
-          <PiecesAndButtons playerColor={playerColor} key="piecesandbuttons"/>
-        </div>
-        <div className={styles.rightPaper}>
-          <h1 className={styles.whosturn}>{urMove ? "It's Your Turn HARDCODEDUSERNAME!" : "It's HARDCODEDOPONENTSNAME's turn"}</h1>
-          <h1 className={styles.youropponent}>Your Opponent: Bobby Fisher</h1>
-          <div className={styles.textChatArea}> text chat area</div>
         </div>
       </div>
     </div>
