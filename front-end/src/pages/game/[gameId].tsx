2d6bac2a8581fb394152150def47ac38582c82fa
Arthur Teatree <71916438+R-33@users.noreply.github.com>
2021-04-12 19:35:37 +0930

    Revert "sleep for waiting for animations to finish"
    This reverts commit f09a90a8a427163f66c135be8157aa85362952f4.

--- front-end/src/pages/game/[gameId].tsx
+++ front-end/src/pages/game/[gameId].tsx
@@ -1,28 +1,15 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import React, { HTMLAttributes, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./gamePage.module.css";
 import { io, Socket } from "socket.io-client";
 import stylesI from "../index.module.css";
 import { Button } from "@material-ui/core";
-import { sleep } from "../../utils/sleep";
 
 const gamePage: NextPage<{ gameId: string }> = ({ gameId }) => {
   const [urMove, setUrMove] = useState<boolean>();
   const [hoverArr, setHoverArr] = useState<number[]>([0, 0, 0, 0, 0, 0, 0]);
-  const [fallingArr, setFallingArr] = useState<number[]>([0, 0, 0, 0, 0, 0, 0]);
-  const [fallingPieceArr, setFallingPieceArr] = useState<JSX.Element[]>([<></>]);
-  const [hoverPieces, setHoverPieces] = useState<JSX.Element[]>([<></>]);
-  const [fallenPieces, setFallenPieces] = useState<JSX.Element[]>([<></>]);
-  const [board, setBoard] = useState<number[][]>([
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-  ]);
-  const [userTurn, setUserTurn] = useState<boolean>(true);
+  const [pieces, setPieces] = useState(<></>);
 
   const socket = io("http://localhost:4000/");
 
@@ -48,20 +35,6 @@ const gamePage: NextPage<{ gameId: string }> = ({ gameId }) => {
             onMouseLeave={() => {
               handleEnterAndLeave(i, false);
             }}
-            onClick={async () => {
-              //TODO: Check if can move
-              const newFArr = fallingArr;
-              newFArr[i] = 1;
-              setFallingArr(newFArr);
-              handlePieceFallingArr("yellow");
-              await sleep(700);
-              setFallingArr([0, 0, 0, 0, 0, 0, 0]);
-              setHoverArr([0, 0, 0, 0, 0, 0, 0]);
-              setFallingPieceArr([<></>])
-              handleFallenPieces();
-
-              //TODO: SEND GAMEBOARD QUERY HERE
-            }}
           />
         </>
       );
@@ -69,52 +42,6 @@ const gamePage: NextPage<{ gameId: string }> = ({ gameId }) => {
     return jsxArr;
   };
 
-  const handleFallenPieces = () => {
-    const newArr = []
-    for (let i = 0; i < 6; i++) {
-      for (let j = 0; j < 7; j++) {
-        if (board[i][j] === 1){
-          console.log(i, j)
-          newArr.push(<img
-            src={`../../../static/red-piece.svg`}
-            alt={`red_piece.svg`}
-            onClick={() => {}}
-            style={{
-              gridColumn: j + 1,
-              gridRow: i + 1,
-            }}
-            key={i + "redfallen" + j}
-            className={styles.fallenPiece}
-          />
-          );
-        }
-        if (board[i][j] === 2) {
-          console.log(i, j)
-          newArr.push(<img
-            src={`../../../static/yellow-piece.svg`}
-            alt={`yellow_piece.svg`}
-            onClick={() => {}}
-            style={{
-              gridColumn: j + 1,
-              gridRow: i + 1,
-            }}
-            key={i + j + "yellowfallen"}
-            className={styles.fallenPiece}
-          />
-          );
-
-        }
-        
-      }
-      
-    }
-    setFallenPieces(newArr)
-  }
-
-
-
-
-  //TODO: Reset state on move because of the placed/ 2 value
   const handleEnterAndLeave = (i: number, enter: boolean) => {
     setHoverArr(
       hoverArr.map((item, index) => {
@@ -129,38 +56,11 @@ const gamePage: NextPage<{ gameId: string }> = ({ gameId }) => {
 
   //TODO: MOVE hoverButtons TO OWN COMPONENET
 
-  const handlePieceFallingArr = (color: string) => {
-    setFallingPieceArr(() => {
-      const addOn = fallingArr.map((item, i) => {
-        if (fallingArr[i] !== 0) {
-          return (
-            <img
-              src={`../../../static/${color}-piece.svg`}
-              alt={`${color}_piece.svg`}
-              onClick={() => {}}
-              style={{
-                gridColumn: i + 1,
-                gridRow: 1,
-              }}
-              key={i + "falling"}
-              className={calculateCSSClass(i)}
-            />
-          );
-        } else {
-          return;
-        }
-      });
-
-      return [...addOn, ...fallingPieceArr];
-    });
-    setFallingArr([0, 0, 0, 0, 0, 0, 0]);
-  };
-
   //TODO: MOVE pieces TO OWN COMPONENET
 
   useEffect(() => {
-    setHoverPieces(
-      hoverArr.map((item, i) => {
+    setPieces(() => {
+      for (let i = 0; i < hoverArr.length; i++) {
         if (hoverArr[i] == 1) {
           return (
             <img
@@ -169,71 +69,18 @@ const gamePage: NextPage<{ gameId: string }> = ({ gameId }) => {
               onClick={() => {}}
               style={{
                 gridColumn: i + 1,
-                gridRow: 1,
               }}
               key={i + "img"}
-              className={styles.fallingPiece}
             />
           );
         }
-      })
-    );
-  }, [hoverArr]);
-
-  const calculateCSSClass = (column: number) => {
-    if (!userTurn){
-      return "error"
-    }
-    setUserTurn(false);
-    let row = null;
-    for (let i = 0; i < 6; i++) {
-      if (board[i + 1]) {
-        if (board[i + 1][column] !== 0 && board[i][column] === 0) {
-          row = i;
-          break;
-        }
-      } else {
-        row = i;
-        break;
-      }
-    }
-
-    const newRow = board[row].map((item, index) => {
-      if (index === column) {
-        return playerNumber;
-      } else {
-        return item;
       }
     });
-    const newArr = board;
-
-    newArr[row] = newRow;
-
-    setBoard(newArr);
-
-    
-
-    switch (row + 1) {
-      case 1:
-        return styles.pieceDown1;
-      case 2:
-        return styles.pieceDown2;
-      case 3:
-        return styles.pieceDown3;
-      case 4:
-        return styles.pieceDown4;
-      case 5:
-        return styles.pieceDown5;
-      case 6:
-        return styles.pieceDown6;
-    }
-    return "f";
-  };
+  }, [hoverArr]);
 
   //TODO: MOVE pieces TO OWN COMPONENET
 
   const playerColor = "red";
-  const playerNumber = 1;
 
   return (
     <div className={stylesI.gradientBG}>
@@ -241,35 +88,14 @@ const gamePage: NextPage<{ gameId: string }> = ({ gameId }) => {
         <div className={styles.boardGrid}>
           <div style={{ gridColumn: 3 }} className={styles.boardUIWrapper}>
             <div className={styles.piecesBoardSeperator}>
-              <div className={styles.piecesHolder}>
-                {fallingPieceArr}
-                {hoverPieces}
-              </div>
-
+              <div className={styles.piecesHolder}>{pieces}</div>
               <img src='../../../static/board.svg' alt='msgSendIco' className={styles.gameBoard} />
-              <div className={styles.pieceArrGrid}>{hoverButtons()} {fallenPieces}</div>
+              <div className={styles.pieceArrGrid}>{hoverButtons()}</div>
             </div>
           </div>
           <div style={{ gridColumn: 5, display: "grid" }} className={styles.sideUIWrapper}>
             <div style={{ gridRow: 1 }} className={styles.playerUIWrapper}>
               <div>{String(hoverArr)}</div>
-              <br />
-              <div>{String(board[0])}</div>
-              <div>{String(board[1])}</div>
-              <div>{String(board[2])}</div>
-              <div>{String(board[3])}</div>
-              <div>{String(board[4])}</div>
-              <div>{String(board[5])}</div>
-              <br />
-              <div>{String(userTurn)}</div>
-              <button
-                onClick={() => {
-                  setUserTurn(userTurn ? false : true);
-                }}
-              >
-                {" "}
-                change turn{" "}
-              </button>
               <div style={{ gridRow: 2 }} className={styles.textChatWrapper}>
                 <div className={styles.sendWrapper}>
                   <input className={styles.textChatArea} />
